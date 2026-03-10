d469ec6d2acf43e084ff69089eca8495265b7d8b
BANK OF AMERICA <mt3741986@gmail.com>
2026-03-10 18:04:08 -0500

    Refactor usuario object to usuarios array

--- data.js
+++ data.js
@@ -1,23 +1,24 @@
-const usuario = {
-  login: "Bank-ofa",
-  correo: "edwinrodrigocungachi@gmail.com",
-  contraseña: "7528",
-  nombre: "EDWIN RODRIGO CUNGACHI TACURI",
-  cuenta: {
-    nombre: "Cuenta Ahorros",
-    numero: "4177 7300 8891 6385",
-    saldo: 17500,
-    creditos: 0,
-    prestamos: 0,
-    movimientos: [
-      { 
-        descripcion: "Depósito inicial", 
-        fecha: "10/03/2026", 
-        monto: "+$17500" 
-      }
-    ]
+const usuarios = [
+  {
+    inicioSesion: "Bank-ofa",
+    correo: "edwinrodrigocungachi@gmail.com",
+    contraseña: "7528",
+    nombre: "EDWIN RODRIGO CUNGACHI TACURI",
+    cuenta: {
+      nombre: "Cuenta Ahorros",
+      numero: "4177 7300 8891 6385",
+      saldo: 17500,
+      creditos: 0,
+      prestamos: 0,
+      movimientos: [
+        { 
+          descripcion: "Depósito inicial", 
+          fecha: "10/03/2026", 
+          monto: "+$17500" 
+        }
+      ]
+    }
   }
-};
 ];
 
 export default usuarios;
