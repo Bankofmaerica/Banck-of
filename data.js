9c92277028debd48458dbd18524a304e17e4d87c
BANK OF AMERICA <mt3741986@gmail.com>
2026-03-10 12:48:48 -0500

    Replace usuarios array with usuario object

--- data.js
+++ data.js
@@ -1,6 +1,23 @@
-const usuarios = [
-    { email: 'user1@example.com', password: 'password1' },
-    { email: 'user2@example.com', password: 'password2' }
+const usuario = {
+  login: "Bank-ofa",
+  correo: "edwinrodrigocungachi@gmail.com",
+  contraseña: "7528",
+  nombre: "EDWIN RODRIGO CUNGACHI TACURI",
+  cuenta: {
+    nombre: "Cuenta Ahorros",
+    numero: "4177 7300 8891 6385",
+    saldo: 17500,
+    creditos: 0,
+    prestamos: 0,
+    movimientos: [
+      { 
+        descripcion: "Depósito inicial", 
+        fecha: "10/03/2026", 
+        monto: "+$17500" 
+      }
+    ]
+  }
+};
 ];
 
 export default usuarios;
